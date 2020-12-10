19c45292733cb99bc65b7fc11bbece443bd66f8c
Arthur Charly <tulokitto1998@gmail.com>
2020-12-09 19:16:55 -0500

    UI - boceto de interfaz de login Arthur Quezada.

--- droidit/app/src/main/java/org/treedev/droidit/MainActivity.kt
+++ droidit/app/src/main/java/org/treedev/droidit/MainActivity.kt
@@ -2,10 +2,24 @@ package org.treedev.droidit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setFragmentByDefault()
     }
+    fun setFragmentByDefault(){
+        changeFragment(LoginFragment())
+    }
+    fun changeFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_fragment,fragment)
+            .commit();
+    }
+
 }
